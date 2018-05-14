b1784943da76af290d548788697ac0e46bdae539
KMárquez <marquezkrodriguez24@gmail.com>
2018-05-14 13:32:03 -0600

    Property Binding, Event Binding y Two Way Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziSquare';
   a = 3;
   b = 14;
+  listo = false;
+  nombre:String = '';
+  apellido:String = '';
+
+  constructor(){
+    setTimeout(() => {
+        this.listo = true
+    },2000)
+  }
+
+  hacerAlgo(){
+    alert("Esto es angular..!!")
+  }
 }
